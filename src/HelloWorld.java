b005c89c661077154f6710bd320f6649a90ed011
wiktorkrzy <wkrzywdz@student.wszib.edu.pl>
2023-11-04 14:31:29 +0100

    Zmiana

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++){
-            System.out.println("HelloWorld");
+
+
         }
 
     }
